06ef16a6ef6f1b7fd42a648e422a1577e383ba1b
Bárbara Pimenta Caetano <bpimentacaetano@gmail.com>
2026-08-13 17:36:47 -0300

    Estrutura inicial

---
+++ src/ler_dados.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+CAMINHO = "./dados/bronze/Spotify.csv"
+
+df = pd.read_csv(CAMINHO)
+
+print(df.shape)
+print(df.head())
+
+for coluna in df.columns:
+    print(f"{coluna}: {df[coluna].dtype}")
+
+
